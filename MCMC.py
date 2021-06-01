638d3fdc088390cb6f553490d63ff47b272417c0
yianliu <yianliu@outlook.com>
2021-06-01 18:30:07 +0100

    Added MCsample for continuous iterations. Need to change the rest!

--- MCMC.py
+++ MCMC.py
@@ -44,11 +44,11 @@ def dH(spins, i, j):
     return H_new - H
 
 # Metropolis-Hastings algorithm for MCMC method
-def MH(n, T, steps):
-    spins = rdspins(n)
+def MH(spins, T, steps):
+    (h, w) = spins.shape
     for step in range(steps):
-        for i in range(n):
-            for j in range(n):
+        for i in range(h):
+            for j in range(w):
                 dh = dH(spins, i, j)
                 if dh <= 0:
                     spins[i, j] = - spins[i, j]
@@ -60,22 +60,38 @@ def MH(n, T, steps):
 
 # Now we create a loop which samples sz spin configurations for a n*n lattice
 # for a range of temperatures which starts at 1 and ends at 3.5 with nt intervals
-# the number of iterations taken to reach the final configurations is itr
+# the number of iterations taken to reach the first configuration is eqsteps
+# and the number of iterations between two configurations is mcsteps
 n = 6
 N = n*n
 sz = 100
 nt = 20
-itr = 100
+eqsteps = 100
+mcsteps = 100
 T_range = np.linspace(1, 3.5, nt)
 # Below are the functions for different thermodynamic properties
-M = np.zeros(nt) # Magnetisation
-E = np.zeros(nt) # Energy
-Cv = np.zeros(nt) # Specific Heat
-X = np.zeros(nt) # Magnetic Susceptibility
+M = np.zeros((nt,sz)) # Magnetisation
+E = np.zeros((nt,sz)) # Energy
+Cv = np.zeros((nt,sz)) # Specific Heat
+X = np.zeros((nt,sz)) # Magnetic Susceptibility
 
 def Mag(spins):
     mag = np.sum(spins) / N
     return mag
+
+def MCsample(n, T, sz, eqsteps, mcsteps):
+    inits = rdspins(n)
+    snapshots = []
+    spins = MH(inits, T, eqsteps)
+    for i in range(sz - 1):
+        spins = MH(spins, T, mcsteps)
+        print(snapshots)
+        snapshots.append(spins)
+        print(snapshots)
+    #return snapshots
+MCsample(6,2.5,3,100,100)
+
+
 
 for i in range(nt):
     t = T_range[i]
