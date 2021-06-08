d4de48b26aaafd1b188d0686ec3aa3a3f707c9ec
yianliu <yianliu@outlook.com>
2021-06-08 09:20:42 +0100

    Completed thermo functions

--- Thermo Functions.py
+++ Thermo Functions.py
@@ -8,6 +8,7 @@ Created on Sat Jun  5 20:38:30 2021
 import numpy as np
 import matplotlib.pyplot as plt
 import copy
+import os
 
 # H is a function which takes a configurationand returns the total Energy
 # of the entire lattice by calculating the Hamiltonian
@@ -35,52 +36,67 @@ def error(lst):
 # E_lst takes a list of spin configurations and returns a list of energy values
 # of those configurations
 def E_lst(spins_multi):
-    E = []
+    Eng = []
     for spins in spins_multi:
-        E.append(H(spins))
-    return np.asarray(E)
-#
-def E_mean(spins_multi):
-    return np.mean(E_lst(spins_multi))
-#
-def E_err(spins_multi):
-    return error(E_lst(spins_multi))
+        Eng.append(H(spins))
+    return np.asarray(Eng)
 
-def E2_lst(spins_multi):
-    E = []
-    for spins in spins_multi:
-        E.append(H(spins)**2)
-    return np.asarray(E)
-#
-def E2_mean(spins_multi):
-    return np.mean(E_lst(spins_multi))
-#
-def E2_err(spins_multi):
-    return error(E_lst(spins_multi))
+# E takes a list of multiple spin configurations and returns a tuple consisting
+# of the mean and the standard error of energy per spin values
+def E(spins_multi):
+    N = spins_multi[0].size
+    Eng = E_lst(spins_multi) / N
+    return (np.mean(Eng), error(Eng))
 
-
-
-
-
-
-
-
-
-
-
-
-
-#
+# M_lst takes a list of spin configurations and returns a list of magnetisation
+# values of those configurations
 def M_lst(spins_multi):
     M = []
     for spins in spins_multi:
-        M.append(Mag(spins))
+        M.append(abs(Mag(spins)))
     return np.asarray(M)
 
-#
-def M_mean(spins_multi):
-    return np.mean(M_lst(spins_multi))
+# M takes a list of multiple spin configurations and returns a tuple consisting
+# of the mean and the standard error of magnetisation per spin values
+def M(spins_multi):
+    N = spins_multi[0].size
+    Mags = M_lst(spins_multi) / N
+    return (np.mean(Mags), error(Mags))
 
-#
-def M_err(spins_multi):
-    return error(M_lst(spins_multi))
+# Cv takes a list of multiple spin configurations and their corresponding
+# temperature and returns the mean and stardard error for the specific heat
+def Cv(spins_multi, T):
+    N = spins_multi[0].size
+    Engs = E_lst(spins_multi)
+    (E_mean, E_err) = E(spins_multi)
+    E2 = []
+    for i in Engs:
+        E2.append(i * i)
+    E2_mean = np.mean(E2)
+    E2_err = error(E2)
+    Cv_mean = (E2_mean - E_mean**2) / (N * T**2)
+    Cv_err = np.sqrt(E2_err**2 + (2 * E_mean * E_err)**2) / (N * T**2)
+    return (Cv_mean, Cv_err)
+
+# X takes a list of multiple spin configurations and their corresponding
+# temperature and returns the mean and stardard error for the magnetic
+# susceptibility
+def X(spins_multi, T):
+    N = spins_multi[0].size
+    Mags = M_lst(spins_multi)
+    (M_mean, M_err) = M(spins_multi)
+    M2 = []
+    for i in Mags:
+        M2.append(i * i)
+    M2_mean = np.mean(M2)
+    M2_err = error(M2)
+    X_mean = (M2_mean - M_mean**2) / (N * T)
+    X_err = np.sqrt(M2_err**2 + (2 * M_mean * M_err)**2) / (N * T)
+    return (X_mean, X_err)
+
+T = 2.32
+data_path = 'Training Data'
+file_name = 'T = ' + format(T, '.2f') + '.npy'
+completeName = os.path.join(data_path, file_name)
+samples = np.load(completeName)
+X(samples, T)
