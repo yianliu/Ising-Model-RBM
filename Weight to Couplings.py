628cffaeb7856b2b5a5cef846fcdf7dd22247788
yianliu <yianliu@outlook.com>
2021-07-08 21:55:18 +0100

    Plotted Couplings

--- Weight to Couplings.py
+++ Weight to Couplings.py
@@ -1,4 +1,5 @@
 from Parameters import *
+from MyFonts import *
 import numpy as np
 import os
 import matplotlib.pyplot as plt
@@ -16,13 +17,13 @@ def weight_to_couplings(W, c):
     return H
 
 
-for nH in nH_list[:1]:
+for nH in nH_list:
     nH_name = 'nH = ' + str(nH)
     save_plot_path = os.path.join('Plots', 'Couplings', nH_name)
     save_couplings_path = os.path.join('Data', 'RBM Parameters', 'Couplings', nH_name)
     weight_path = os.path.join('Data', 'RBM Parameters', 'Weights', nH_name)
     h_bias_path = os.path.join('Data', 'RBM Parameters', 'Hid Biases', nH_name)
-    for T in T_range[:1]:
+    for T in T_range:
         H_arr = np.zeros((num_snapshots, 64, 64))
         file_name = 'T = ' + format(T, '.2f') + '.npy'
         interval_epochs = int(me / num_snapshots)
@@ -34,39 +35,21 @@ for nH in nH_list[:1]:
         #     H_arr[snapshot] = weight_to_couplings(W, c)
         # np.save(os.path.join(save_couplings_path, file_name), H_arr)
         #
-        H_arr = np.load(os.path.join(save_couplings_path, file_name)
+        H_arr = np.load(os.path.join(save_couplings_path, file_name))
         fig, axes = plt.subplots(nrows = 2, ncols = 3, figsize=(10,7))
         for i, ax in enumerate(fig.axes):
             snapshot = snapshot_list[i]
             epochs_snapshot = (snapshot + 1) * interval_epochs
             epoch_name = 'Epochs = ' + str(epochs_snapshot)
             H = H_arr[snapshot]
-            ax.imshow(H)
-
-
-nH = 64
-T = 2.5
-nH_name = 'nH = ' + str(nH)
-save_plot_path = os.path.join('Plots', 'Couplings', nH_name)
-save_couplings_path = os.path.join('Data', 'RBM Parameters', 'Couplings', nH_name)
-weight_path = os.path.join('Data', 'RBM Parameters', 'Weights', nH_name)
-h_bias_path = os.path.join('Data', 'RBM Parameters', 'Hid Biases', nH_name)
-H_arr = np.zeros((num_snapshots, 64, 64))
-file_name = 'T = ' + format(T, '.2f') + '.npy'
-interval_epochs = int(me / num_snapshots)
-# for snapshot in range(num_snapshots):
-#     epochs_snapshot = (snapshot + 1) * interval_epochs
-#     epoch_name = 'Epochs = ' + str(epochs_snapshot)
-#     W = np.load(os.path.join(weight_path, epoch_name, file_name))
-#     c = np.load(os.path.join(h_bias_path, epoch_name, file_name))
-#     H_arr[snapshot] = weight_to_couplings(W, c)
-# np.save(os.path.join(save_couplings_path, file_name), H_arr)
-#
-H_arr = np.load(os.path.join(save_couplings_path, file_name)
-
-for i in range(6):
-    snapshot = snapshot_list[i]
-    epochs_snapshot = (snapshot + 1) * interval_epochs
-    epoch_name = 'Epochs = ' + str(epochs_snapshot)
-    H = H_arr[snapshot]
-    print(H)
+            im = ax.imshow(H, label = epoch_name)
+            ax.set_xticks([])
+            ax.set_yticks([])
+            ax.set_title(epoch_name)
+        fig.suptitle('T = ' + format(T, '.2f'), size = 18, y = 0.95)
+        plt.tight_layout(pad = 3)
+        fig.colorbar(im, ax = axes)
+        figname = os.path.join(save_plot_path, 'T = ' + format(T, '.2f') + '.jpg')
+        if os.path.isfile(figname):
+           os.remove(figname)
+        fig.savefig(figname, dpi = 1200)
